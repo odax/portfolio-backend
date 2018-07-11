198190cad57307ce2ec4c881b69794ccfa2796e8
odax <nr10@albion.edu>
2018-07-10 20:12:13 -0400

    undoed adding json to return the req to the client

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ server.post('/main', function(req, res) {
     };
     sendgrid.send(email)
     .then(() => {
-        res.status(200).json({req}).end();
+        res.status(200).end();
     })
     .catch((err) => {
             res.status(500).json({error: err}).end();
